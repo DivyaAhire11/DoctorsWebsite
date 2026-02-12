af7bf47922567469448c06c385ed57e666a45c28
DivyaAhire11 <divyaahire121@gmail.com>
2026-02-12 19:56:25 +0530

    db file modification

--- config/db.php
+++ config/db.php
@@ -1,10 +1,14 @@
 <?php
-$con = pg_connect("
- host=localhost
- port=5432
- dbname=hospital 
- user=postgres
- password=tybcs");
+
+$host = "localhost";
+$port = "5432";
+$dbname = "hospital";
+$user = "postgres";
+$password = getenv("DB_PASSWORD");
+
+
+
+$con = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
 if (!$con) {
    die("Database connection failed");
